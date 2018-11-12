4a3a0eb5bc989955957cb1c0f4aee50bcaf53516
Jordan <jordanhaddadi@gmail.com>
2018-11-12 17:45:17 -0500

    added search term response

--- server.js
+++ server.js
@@ -8,21 +8,36 @@ const data = require('./db/notes');
 
 const app = express();
 
-// const stringData = data.toString();
-
 // ADD STATIC SERVER HERE
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
-    res.json(data);
-  });
 
 app.get('/api/notes/:id', (req, res) => {
-
+    //let's us use numbers 
     const id = Number(req.params.id);
+    //finds item by ID
     const bookmark = data.find(item => item.id === id);
+
     res.json(bookmark);
 });
+
+
+ // http://localhost:8080/api/notes?searchTerm=cats
+  //http://localhost:8080/api/notes/:searchTerm
+
+app.get('/api/notes', (req, res) => {
+    //lets us retrieve the searchTerm from the query.string on req.query object
+    const searchTerm = req.query.searchTerm;
+    console.log(searchTerm);
+    //filter through the array from searchterm and return results
+    if (searchTerm) {
+        let filtered = data.filter(item => item.title.includes(searchTerm));
+        res.json(filtered);
+    } else {
+        res.json(data);
+    }
+});
+
 
 app.listen(8080, function () {
     console.info(`Server listening on ${this.address().port}`);
