a999cde91ae48c38c1555a21995700b669e57eda
Jordan <jordanhaddadi@gmail.com>
2018-11-12 15:49:29 -0500

    adding endpoint for ID

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 // Load array of notes
@@ -7,14 +8,27 @@ const data = require('./db/notes');
 
 const app = express();
 
-// ADD STATIC SERVER HERE
+// const stringData = data.toString();
 
-app.listen(8080, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
+// ADD STATIC SERVER HERE
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(data);
+  });
+
+app.get('/api/notes/:id', (req, res) => {
+
+    const id = Number(req.params.id);
+    const bookmark = data.find(item => item.id === id);
+    res.json(bookmark);
 });
 
+app.listen(8080, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
 
 console.log('Hello Noteful!');
 
